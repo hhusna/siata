9195986cb38c62eb326f882e13c6f8582055f328
BPR ‎ <222313021@stis.ac.id>
2025-11-12 21:36:05 +0700

    add configuration to pom.xml

--- client/src/main/java/MainApplication.java
+++ client/src/main/java/MainApplication.java
@@ -1,6 +1,19 @@
-public class MainApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("JavaFX plugin works!");
+        Scene scene = new Scene(label, 300, 200);
+        stage.setTitle("JavaFX Test");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-
+        launch();
     }
 }
